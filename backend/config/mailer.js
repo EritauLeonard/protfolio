b1abfa47c0d6cc332eb4333c84261a98bf86837b
EritauLeonard <razafimahatradraibeeritauleina@gmail.com>
2026-09-12 21:20:52 -0900

    Modification de mailer.js

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -2,11 +2,14 @@ const nodemailer = require("nodemailer");
 
 function createTransporter() {
   return nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true pour le port 465
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    family: 4, // force IPv4 — corrige le timeout sur Render
   });
 }
 
